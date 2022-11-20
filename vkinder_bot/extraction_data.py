8e79f8c2c288ce8c5def226a9bac52551eb45d6f
stirelshka8 <stirelshka8@gmail.com>
2022-11-20 08:08:36 +0300

    Метод поиска

--- vkinder_bot/extraction_data.py
+++ vkinder_bot/extraction_data.py
@@ -17,6 +17,19 @@ class ExtractingUserData:
         self.country = country
 
     def user_search(self):
+        """
+        Метод поиска, получает на вход параметры:
+        count - количество найденых записей (не более 999)
+        age_from - от какого возраста искать
+        age_to - до какого возраста искать
+        sex - пол (2 мужчина, 1 женщина)
+        city - идентификаттор города (берется у пользователя который ведет диалог с ботом)
+        country - идентификатор страны (берется у пользователя который ведет диалог с ботом)
+
+        Поиск ведется ТОЛЬКО по страницам пользователей у которых установлен смейный статус "В активном поиске"
+        :return:
+        """
+
         self.paramitres = {'access_token': self.token, 'count': self.count, 'has_photo': 1, 'age_from': self.age_from,
                            'age_to': self.age_to, 'fields': 'photo_200_orig, relation: 6', 'sex': self.sex,
                            'city': self.city, 'country': self.country, 'v': 5.131}
