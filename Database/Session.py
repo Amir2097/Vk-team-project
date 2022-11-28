b02e46e188c0b9da8c4c9d070c9cb05d375ff075
Saifulin Nail <saifulin-nail@mail.ru>
2022-11-28 20:00:48 +0500

    изменения в session и extraction_data

--- Database/Session.py
+++ Database/Session.py
@@ -1,17 +1,86 @@
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
 import configparser
-from models import create_tables, User, Favorite, Blocked, Founduser, Photo
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
-config = configparser.ConfigParser()
-config.read('config_bot.cfg')
-db_user = config.get('DATABASE', 'db_user')
-db_password = config.get('DATABASE', 'db_password')
-db_host = config.get('DATABASE', 'db_host')
+Base = declarative_base()
 
-DSN = "postgresql://%s:%s@%s:5432/vkinder" % (db_user, db_password, db_host)
-engine = sqlalchemy.create_engine(DSN)
-create_tables(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+class Mainuser(Base):
+    __tablename__ = "mainuser"
+
+    user_id = sq.Column(sq.Integer, primary_key=True)
+    vk_id = sq.Column(sq.String(length=40), unique=True)
+    name = sq.Column(sq.String(length=40))
+    lastname = sq.Column(sq.String(length=40))
+    date_of_birth = sq.Column(sq.DateTime)
+
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+
+    favorite_id = sq.Column(sq.Integer, primary_key=True)
+    vk_id = sq.Column(sq.String(length=40), nullable=False)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey("mainuser.user_id"), nullable=False)
+    Mainuser = relationship(Mainuser, backref="favorites")
+
+
+class Blocked(Base):
+    __tablename__ = "blocked"
+
+    blocked_id = sq.Column(sq.Integer, primary_key=True)
+    vk_id = sq.Column(sq.String(length=40), unique=True, nullable=False)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey("mainuser.user_id"), nullable=False)
+    Mainuser = relationship(Mainuser, backref="blocking")
+
+
+class Founduser(Base):
+    __tablename__ = "founduser"
+
+    found_user_id = sq.Column(sq.Integer, primary_key=True)
+    vk_id = sq.Column(sq.String(length=40), unique=True)
+    name = sq.Column(sq.String(length=40))
+    lastname = sq.Column(sq.String(length=40))
+    user_id = sq.Column(sq.Integer, sq.ForeignKey("mainuser.user_id"), nullable=False)
+    Mainuser = relationship(Mainuser, backref="foundusers")
+
+
+class Photo(Base):
+    __tablename__ = "photo"
+
+    photo_id = sq.Column(sq.Integer, primary_key=True)
+    link = sq.Column(sq.String(length=40), nullable=False)
+    quantity_like = sq.Column(sq.Integer)
+    found_user_id = sq.Column(sq.Integer, sq.ForeignKey("founduser.found_user_id"), nullable=False)
+    Founduser = relationship(Founduser, backref="foundusers")
+
+
+def create_tables(engine):
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+
+class Connect:
+    """Подключение к бд и создание сессии"""
+
+    config = configparser.ConfigParser()
+    config.read("../config_bot.cfg")
+    db_user = config.get('DATABASE', 'db_user')
+    db_password = config.get('DATABASE', 'db_password')
+    db_host = config.get('DATABASE', 'db_host')
+
+    DSN = "postgresql://%s:%s@%s:5432/vkinder" % (db_user, db_password, db_host)
+    engine = sq.create_engine(DSN)
+    create_tables(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    def user_database_entry(self, data):
+        """Метод принимает json из функции profile_info и добавляет информацию в бд о пользователе,
+        зарегестрировавшемся в боте"""
+
+        new_post = Mainuser(vk_id=data.get('id'), name=data.get('first_name'), lastname=data.get('last_name'),
+                            date_of_birth=data.get('ddate'))
+        self.session.add(new_post)
+        self.session.commit()
+
