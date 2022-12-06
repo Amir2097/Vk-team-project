c340dffbb931a224188d0d7b18a148d39ad5710a
stirelshka8 <stirelshka8@gmail.com>
2022-12-06 20:26:45 +0300

    Времечко)

--- vkinder_bot/bot.py
+++ vkinder_bot/bot.py
@@ -1,3 +1,4 @@
+from datetime import time
 from random import randrange
 import configparser
 import vk_api, json
@@ -94,6 +95,27 @@ sex_keyboard = get_keyboard([
 
 
 def run_bot():
+    second_time = 0
+    minut_time = 0
+    hour_time = 0
+    count_day = 0
+    while True:
+        time.sleep(1)
+        second_time += 1
+        if second_time == 60:
+            second_time = 0
+            minut_time += 1
+            if minut_time == 60:
+                minut_time = 0
+                hour_time += 1
+                if hour_time == 24:
+                    hour_time = 0
+                    count_day += 1
+
+        print(
+            f"\rВремя работы БОТА: Дни - {count_day} Часы - {hour_time} Минуты - {minut_time} Секунды - {second_time}",
+            end='')
+
     global age_from, age_to, user_sex
 
     def write_msg(user_id, message, keyboard=None):
